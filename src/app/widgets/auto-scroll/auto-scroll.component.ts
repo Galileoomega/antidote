3be81a4033b629cee15131ee816ad8e2de474c3d
galileoomega <alexis.limacher@outlook.com>
2025-02-27 18:30:21 +0100

    struct:

---
+++ src/app/widgets/auto-scroll/auto-scroll.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-scroll',
+  imports: [],
+  templateUrl: './auto-scroll.component.html',
+  styleUrl: './auto-scroll.component.scss'
+})
+export class AutoScrollComponent {
+
+}
