81da188092faa7ca8749efa65b85046c2bbcd6c2
galileoomega <alexis.limacher@outlook.com>
2025-02-27 19:01:34 -0800

    temp carousel

--- src/app/interface/project-infos/project-infos.component.ts
+++ src/app/interface/project-infos/project-infos.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-project-infos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-infos.component.html',
   styleUrl: './project-infos.component.scss'
 })
 export class ProjectInfosComponent {
-
+  
 }
