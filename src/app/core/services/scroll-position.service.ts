91715917e282a751d2e3cb2d3cfacedc5102aa77
galileoomega <alexis.limacher@outlook.com>
2025-04-02 08:31:18 -0700

    dsadas

--- src/app/core/services/scroll-position.service.ts
+++ src/app/core/services/scroll-position.service.ts
@@ -8,6 +8,8 @@ import { filter, take } from 'rxjs/operators';
 export class ScrollPositionService {
   public scrollPositions = new Map<string, number>();
 
+  navCount = 0
+
   constructor(private router: Router, private ngZone: NgZone) {
     if (typeof window !== 'undefined') {
       window.history.scrollRestoration = 'manual';
@@ -18,10 +20,19 @@ export class ScrollPositionService {
           this.ngZone.onStable
             .pipe(take(1))
             .subscribe(() => {
+              this.navCount++;
+
+              if(this.navCount > 2) {
+                this.clearScrollPosition();
+                this.navCount = 0;
+              }
+
               const savedScroll = this.scrollPositions.get(this.router.url);
+
               if (savedScroll != null) {
                 window.scrollTo(0, savedScroll);
                 this.clearScrollPosition();
+                this.navCount = 0;
               } else {
                 window.scrollTo(0, 0);
               }
