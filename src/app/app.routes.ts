f3e09a94f4097dfdcb97d5cb457a065a3e77c7df
galileoomega <your.email@example.com>
2025-04-15 05:08:36 -0700

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ContactComponent } from './features/contact/contact.component';
 import { ProjectsComponent } from './features/projects/projects.component';
 import { ProjectInfosComponent } from './features/project-infos/project-infos.component';
 import { delayResolver } from './core/resolvers/delay.resolver';
+import { AboutComponent } from './features/about/about.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent, resolve: {delay: delayResolver} },
     { path: "contact", component: ContactComponent, resolve: {delay: delayResolver} },
     { path: "projects", component: ProjectsComponent, resolve: {delay: delayResolver} },
+    { path: "about", component: AboutComponent, resolve: {delay: delayResolver} },
     { path: "project/:id", component: ProjectInfosComponent, resolve: {delay: delayResolver} },
     { path: '**', redirectTo: '' }
 ];
